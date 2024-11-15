b8b92390c420c0e56afa651209d5963cf612e980
Pasupathikumar <kumarmarimuthu99@gmail.com>
2024-11-15 12:22:11 +0530

    commit

--- client/src/api/AuthRequests.js
+++ client/src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://48.216.181.152:5000' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const logIn= (formData)=> API.post('/auth/login',formData);
 
--- client/src/api/MessageRequests.js
+++ client/src/api/MessageRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://48.216.181.152:5000' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 
