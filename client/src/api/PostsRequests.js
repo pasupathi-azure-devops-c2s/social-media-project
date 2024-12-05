20661739aa800dd13cd7c8ccf5b36a042f46d8e4
Pasupathikumar <kumarmarimuthu99@gmail.com>
2024-12-05 17:32:00 +0530

    sa

--- client/src/api/PostsRequests.js
+++ client/src/api/PostsRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://13.201.11.1:5000' });
+const API = axios.create({ baseURL: 'http://172.212.58.96:5000/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
